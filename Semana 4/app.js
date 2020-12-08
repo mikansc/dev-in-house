b9fd3b3a8e1096476332912a3b6a93e447d7e40b
mikansc <michael.nsc@outlook.com>
2020-12-08 17:06:17 -0300

    ex1 calculo media de notas

--- Semana 4/app.js
+++ Semana 4/app.js
@@ -1,30 +1,8 @@
-var arr = [2, 3, 4];
-
-function arrayDeNumerosVar(array) {
-  var num = 2;
-  for (var i = 0; i < array.length; i++) {
-    var soma = soma + array[i];
-  }
-  console.log(soma);
-}
-
-arrayDeNumerosVar(arr);
-
-// Sobre var x let ...
-
-function hoistingComVar() {
-  console.log(age);
-  if (true) {
-    var age = 29;
-  }
-}
-function hoistingComLet() {
-  console.log(year);
-  if (true) {
-    let year = 1991;
-  }
-}
-hoistingComVar(); // o que esta função retorna?
-hoistingComLet(); // e esta função?
-
-// Pq que as funções apresentam comportamentos diferentes?
+// [SP04] 1 - Cálculo de Média de Notas
+const arrayDeNotas = [5, 10, 5, 5, 5];
+const mediaNotas = (listaNotas) => {
+  let soma = listaNotas.reduce((acc, nota) => acc + nota);
+  let qtd = listaNotas.length;
+  console.log("Média: ", soma / qtd);
+};
+mediaNotas(arrayDeNotas);
