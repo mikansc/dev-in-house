6db89e457965707713e95e6daee95cca9a591081
mikansc <michael.nsc@outlook.com>
2020-12-08 17:45:30 -0300

    [SP04] 8 - Multiplicação de Valores da Lista

--- Semana 4/app.js
+++ Semana 4/app.js
@@ -1,50 +1,9 @@
-// [SP04] 7 - Encontrar uma pessoa de maior
-const lista = [
-  {
-    nome: "Michael",
-    idade: 29,
-    telefone: "234-5678",
-    profissao: "Programador",
-  },
-  {
-    nome: "João",
-    idade: 17,
-    telefone: "234-5678",
-    profissao: "Programador",
-  },
-  {
-    nome: "Maria",
-    idade: 21,
-    telefone: "234-5678",
-    profissao: "Programador",
-  },
-];
+// [SP04] 8 - Multiplicação de Valores da Lista
 
-const verificarMaioridade = (listaDePessoas) => {
-  // retorna o objeto da pessoa maior de idade
-  return listaDePessoas.filter((pessoa) => pessoa.idade > 18);
-  // retorna true se alguém for maior de idade
-  // return listaDePessoas.some((pessoa) => pessoa.idade > 18);
+const lista = [1, 2, 3, 4, 5];
+
+const multiplicarValores = (listaDeValores) => {
+  return listaDeValores.reduce((acc, valor) => acc * valor, 1);
 };
 
-const saoTodosProgramadores = (listaDePessoas) => {
-  return listaDePessoas.every((pessoa) => pessoa.profissao === "Programador");
-};
-
-const listarNomes = (listaDePessoas) => {
-  return listaDePessoas.map((pessoa) => pessoa.nome);
-};
-
-const soMenoresDeIdade = (listaDePessoas) => {
-  return listaDePessoas.filter((pessoa) => pessoa.idade < 18);
-};
-
-const primeiraComMaisDeDezoito = (listaDePessoas) => {
-  return listaDePessoas.filter((pessoa) => pessoa.idade > 18)[0];
-};
-
-// console.log(verificarMaioridade(lista));
-// console.log(saoTodosProgramadores(lista));
-// console.log(listarNomes(lista));
-// console.log(soMenoresDeIdade(lista));
-console.log(primeiraComMaisDeDezoito(lista));
+console.log(multiplicarValores(lista));
