302630771fed2db03279ef290c8f53fca8f69e45
mikansc <michael.nsc@outlook.com>
2020-12-08 17:10:45 -0300

    [SP04] - 2 - Quadrado de valores

--- Semana 4/app.js
+++ Semana 4/app.js
@@ -1,8 +1,17 @@
 // [SP04] 1 - Cálculo de Média de Notas
-const arrayDeNotas = [5, 10, 5, 5, 5];
-const mediaNotas = (listaNotas) => {
-  let soma = listaNotas.reduce((acc, nota) => acc + nota);
-  let qtd = listaNotas.length;
-  console.log("Média: ", soma / qtd);
+// const arrayDeNotas = [5, 10, 5, 5, 5];
+// const mediaNotas = (listaNotas) => {
+//   let soma = listaNotas.reduce((acc, nota) => acc + nota);
+//   let qtd = listaNotas.length;
+//   console.log("Média: ", soma / qtd);
+// };
+// mediaNotas(arrayDeNotas);
+
+// [SP04] - 2 - Quadrado de valores
+const arrayDeNumeros = [2, 3, 4, 11];
+const quadradoDeCadaNumero = (listaDeNumeros) => {
+  listaDeNumeros.forEach((num) => {
+    console.log("O quadrado de", num, "é", num ** 2);
+  });
 };
-mediaNotas(arrayDeNotas);
+quadradoDeCadaNumero(arrayDeNumeros);
