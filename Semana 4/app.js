34e79ba7997a9166b291714fca400e4be1b5fc00
mikansc <michael.nsc@outlook.com>
2020-12-09 18:59:59 -0300

    [SP04] 13 - Filtrar Cidades

--- Semana 4/app.js
+++ Semana 4/app.js
@@ -11,4 +11,9 @@ const filtrarCidadesDeSC = (listaDeCidades) => {
   return listaDeCidades.filter((cidade) => cidade.estado === "SC");
 };
 
+const concatenaCidadeEstado = (listaDeCidades) => {
+  return listaDeCidades.map((cidade) => `${cidade.nome}, ${cidade.estado}.`);
+};
+
 console.log(filtrarCidadesDeSC(lista));
+console.log(concatenaCidadeEstado(lista));
