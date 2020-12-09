43265fd3f2de999f7a69349fea4abe1447230b6c
mikansc <michael.nsc@outlook.com>
2020-12-09 18:37:22 -0300

    [SP04] 11 - Cálculo de área de um Retângulo

--- Semana 4/app.js
+++ Semana 4/app.js
@@ -1,15 +1,9 @@
-// [SP04] 10 - Soma com resultado Par
+// [SP04] 11 - Cálculo de área de um Retângulo
 
-const somaComResultadoPar = (num1, num2) => {
-  return new Promise((resolve, reject) => {
-    let soma = num1 + num2;
-    if (soma % 2 === 0) {
-      resolve("Resultado é par");
-    } else {
-      reject("Resultado é ímpar");
-    }
-  });
+const retangulo = { base: 4, altura: 12 };
+
+const areaRetangulo = (objRetangulo) => {
+  return objRetangulo.base * objRetangulo.altura;
 };
 
-somaComResultadoPar(2, 4).then((res) => console.log(res));
-somaComResultadoPar(2, 3).catch((err) => console.log(err));
+console.log(areaRetangulo(retangulo));
