649a9eac45e5aa67f3f6ece0adce8f5e881e7dd5
mikansc <michael.nsc@outlook.com>
2021-02-05 23:10:06 -0300

    [SP09] 10 - Crie uma tabela de associação... (final)

--- Semana 9/respostas.sql
+++ Semana 9/respostas.sql
@@ -237,3 +237,52 @@ SELECT *
 FROM estudantes 
 WHERE media_geral > 7.0 
 ORDER BY media_geral DESC;
+
+CREATE TABLE estudantes_cursos
+(
+	estudante_matricula INT NOT NULL,
+  	curso_codigo INT NOT NULL,
+  	media_curso DECIMAL(5,2)
+);
+
+INSERT INTO estudantes_cursos (estudante_matricula, curso_codigo, media_curso)
+VALUES (
+	10001,
+  30001,
+  8.5
+),
+(
+	10001,
+  30003,
+  7.0
+),
+(
+	10002,
+  30003,
+  9.20
+),
+(
+	10003,
+  30005,
+  8.75
+),
+(
+	10004,
+  30005,
+  8.0
+),
+(
+	10004,
+  30003,
+  9.5
+),
+(
+	10005,
+  30004,
+  9.99
+),
+(
+	10005,
+  30001,
+  9.99
+)
