744c716ab53f82a1f02ccb9b71a00ae796544985
mikansc <michael.nsc@outlook.com>
2021-02-05 22:46:24 -0300

    [SP09] 6 - Exclua um curso. (final)

---
+++ Semana 9/ex6.sql
@@ -0,0 +1,1 @@
+DELETE FROM cursos WHERE nome = "DEVInHouse React";
--- Semana 9/respostas.sql
+++ Semana 9/respostas.sql
@@ -217,3 +217,5 @@ VALUES (
 SELECT * FROM cursos WHERE `EAD`= false AND max_alunos > 30;
 
 UPDATE estudantes SET media_geral = 8.75 WHERE nome = "Augusto" AND sobrenome = "Otavio";
+
+DELETE FROM cursos WHERE nome = "DEVInHouse React";
