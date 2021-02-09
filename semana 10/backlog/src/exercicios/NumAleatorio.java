a1a3f0b3ef3b63b6015ede46a392b1521cfec61a
mikansc <michael.nsc@outlook.com>
2021-02-09 11:33:47 -0300

    [SP10] Exercício 3

---
+++ semana 10/backlog/src/exercicios/NumAleatorio.java
@@ -0,0 +1,9 @@
+package exercicios;
+
+import java.util.Random;
+
+public class NumAleatorio {
+    public static void main(String[] args) {
+        System.out.println(new Random().nextInt(2));
+    }
+}
